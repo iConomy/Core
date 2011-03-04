5b40147f8c827c7a244aec6ce74863542dea2f52
Nijiko Yonskai <me@nijikokun.com>
2011-03-04 14:27:24 -0500

    Added error on invalid number, saving upon adding, interest logging.

--- src/com/nijiko/coelho/iConomy/system/Interest.java
+++ src/com/nijiko/coelho/iConomy/system/Interest.java
@@ -13,22 +13,35 @@ public class Interest extends TimerTask {
     @Override
     public void run() {
         DecimalFormat DecimalFormat = new DecimalFormat("#.##");
+        double amount = 0.0;
 
-        double amount = (Constants.Interest_FlatRate == 0.0) ? Double.parseDouble(
-            DecimalFormat.format(
-                Math.random() * (
-                    Constants.Interest_Max_Interval - Constants.Interest_Min_Interval
-                ) + (
-                    Constants.Interest_Min_Interval
+        try {
+            amount = (Constants.Interest_FlatRate == 0.0) ? Double.valueOf(
+                DecimalFormat.format(
+                    Math.random() * (
+                        Constants.Interest_Max_Interval - Constants.Interest_Min_Interval
+                    ) + (
+                        Constants.Interest_Min_Interval
+                    )
                 )
-            )
-        ) : Constants.Interest_FlatRate;
+            ).doubleValue() : Constants.Interest_FlatRate;
+        } catch (NumberFormatException e) {
+            System.out.println("[iConomy] Invalid Interest: " + e);
+        }
 
         Player players[] = iConomy.getBukkitServer().getOnlinePlayers();
 
         for (Player p : players) {
             if (iConomy.getBank().hasAccount(p.getName())) {
-                iConomy.getBank().getAccount(p.getName()).add(amount);
+                Account account = iConomy.getBank().getAccount(p.getName());
+                account.add(amount);
+                account.save();
+                
+                if(amount < 0.0)
+                    iConomy.getTransactions().insert("[System Interest]", p.getName(), 0.0, account.getBalance(), 0.0, 0.0, amount);
+                else {
+                    iConomy.getTransactions().insert("[System Interest]", p.getName(), 0.0, account.getBalance(), 0.0, amount, 0.0);
+                }
             }
         }
     }
