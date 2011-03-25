82d211d78caf9bcea130e7dc43c3ff4a40fe8881
Nijiko Yonskai <me@nijikokun.com>
2011-03-25 17:38:18 -0400

    Removed database pooling as it was rolling back MySQL data.

--- src/com/nijiko/coelho/iConomy/net/Database.java
+++ src/com/nijiko/coelho/iConomy/net/Database.java
@@ -3,49 +3,51 @@ package com.nijiko.coelho.iConomy.net;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import com.nijiko.coelho.iConomy.util.Constants;
 import com.nijiko.coelho.iConomy.util.Misc;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Database {
-
-    private Connection Connection;
-    private Statement Stmt;
-    private PreparedStatement Statement;
-    private ResultSet ResultSet;
-    private ConnectionPool Pool = null;
+    private String driver;
+    private String dsn;
+    private String username;
+    private String password;
 
     public Database() {
-        if(Pool == null) {
-            if(Misc.is(Constants.Database_Type, new String[] { "sqlite", "h2", "h2sql" })) {
-                Pool = new ConnectionPool(
-                    "org.h2.Driver",
-                    "jdbc:h2:" + Constants.Plugin_Directory + File.separator + Constants.SQL_Database + ";AUTO_RECONNECT=TRUE;FILE_LOCK=SERIALIZED",
-                    "sa",
-                    "sa"
-                );
-            } else if (Constants.Database_Type.equalsIgnoreCase("mysql")) {
-                Pool = new ConnectionPool(
-                    "com.mysql.jdbc.Driver",
-                    "jdbc:mysql://" + Constants.SQL_Hostname + ":" + Constants.SQL_Port + "/" + Constants.SQL_Database,
-                    Constants.SQL_Username,
-                    Constants.SQL_Password
-                );
-            }
+        if(Misc.is(Constants.Database_Type, new String[] { "sqlite", "h2", "h2sql" })) {
+            driver = "org.h2.Driver";
+            dsn = "jdbc:h2:" + Constants.Plugin_Directory + File.separator + Constants.SQL_Database + ";AUTO_RECONNECT=TRUE;FILE_LOCK=SERIALIZED";
+            username = "sa";
+            password = "sa";
+        } else if (Constants.Database_Type.equalsIgnoreCase("mysql")) {
+            driver = "com.mysql.jdbc.Driver";
+            dsn = "jdbc:mysql://" + Constants.SQL_Hostname + ":" + Constants.SQL_Port + "/" + Constants.SQL_Database;
+            username = Constants.SQL_Username;
+            password = Constants.SQL_Password;
         }
+
+        try {
+            Class.forName(driver).newInstance();
+        } catch (Exception e) { System.out.println("[iConomy] Driver error: " + e); }
     }
 
     public Connection checkOut() {
-        return Pool.checkOut();
+        try {
+            if(username.equalsIgnoreCase("") && password.equalsIgnoreCase(""))
+                return (DriverManager.getConnection(dsn));
+            else {
+                return (DriverManager.getConnection(dsn, username, password));
+            }
+        } catch (SQLException e) {
+            System.out.println("[iConomy] Could not create connection: " + e);
+            return (null);
+        }
     }
 
     public void checkIn(Connection connection) {
-        this.Pool.checkIn(connection);
+        try {
+            connection.close();
+        } catch (SQLException ex) {}
     }
 }
