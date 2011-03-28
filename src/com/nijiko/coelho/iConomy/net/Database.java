f64508c9fdb0fb67dd746ecdb1efa181564f3845
Nijiko Yonskai <me@nijikokun.com>
2011-03-27 22:36:05 -0400

    Grab connection pool to empty it.

--- src/com/nijiko/coelho/iConomy/net/Database.java
+++ src/com/nijiko/coelho/iConomy/net/Database.java
@@ -62,4 +62,8 @@ public class Database {
             connection.close();
         } catch (SQLException ex) {}
     }
+
+    public JdbcConnectionPool connectionPool() {
+        return h2pool;
+    }
 }
