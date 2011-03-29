46b645a23bfdbd90a4535cfce82e669fa04f84ab
Nijiko Yonskai <me@nijikokun.com>
2011-03-29 12:39:28 -0400

    Fixed transactions, databases are now created and everything runs smooth.

--- src/com/nijiko/coelho/iConomy/system/Transactions.java
+++ src/com/nijiko/coelho/iConomy/system/Transactions.java
@@ -12,8 +12,7 @@ import java.sql.SQLException;
 
 public class Transactions {
 
-    public Transactions() {
-    }
+    public Transactions() { }
 
     public void load() throws Exception {
         Connection conn = null;
@@ -21,19 +20,19 @@ public class Transactions {
         PreparedStatement ps = null;
 
         if (Constants.Log_Data) {
+            conn = iConomy.getDatabase().getConnection();
+
             if (Misc.is(Constants.Database_Type, new String[] { "sqlite", "h2", "h2sql" })) {
                 try {
                     ps = conn.prepareStatement("CREATE TABLE " + Constants.SQL_Table + "_Transactions(id INT AUTO_INCREMENT PRIMARY KEY, account_from TEXT, account_to TEXT, account_from_balance DECIMAL(65, 2), account_to_balance DECIMAL(65, 2), timestamp TEXT , set DECIMAL(65, 2), gain DECIMAL(65, 2), loss DECIMAL(65, 2));");
                     ps.executeUpdate();
                 } catch(SQLException E) { }
             } else {
-                conn = iConomy.getDatabase().checkOut();
                 DatabaseMetaData dbm = conn.getMetaData();
                 rs = dbm.getTables(null, null, Constants.SQL_Table + "_Transactions", null);
 
                 if (!rs.next()) {
                     System.out.println("[iConomy] Creating logging database.. [" + Constants.SQL_Table + "_Transactions]");
-
                     ps = conn.prepareStatement("CREATE TABLE " + Constants.SQL_Table + "_Transactions (`id` INT(255) NOT NULL AUTO_INCREMENT, `account_from` TEXT NOT NULL, `account_to` TEXT NOT NULL, `account_from_balance` DECIMAL(65, 2) NOT NULL, `account_to_balance` DECIMAL(65, 2) NOT NULL, `timestamp` TEXT NOT NULL, `set` DECIMAL(65, 2) NOT NULL, `gain` DECIMAL(65, 2) NOT NULL, `loss` DECIMAL(65, 2) NOT NULL, PRIMARY KEY (`id`))");
 
                     if(ps != null) {
@@ -54,7 +53,7 @@ public class Transactions {
             try { rs.close(); } catch (SQLException ex) { }
 
         if(conn != null)
-            iConomy.getDatabase().checkIn(conn);
+            iConomy.getDatabase().close(conn);
     }
 
     /**
@@ -80,7 +79,7 @@ public class Transactions {
         PreparedStatement ps = null;
 
         try {
-            conn = iConomy.getDatabase().checkOut();
+            conn = iConomy.getDatabase().getConnection();
             ps = conn.prepareStatement("INSERT INTO " + Constants.SQL_Table + "_Transactions(account_from, account_to, account_from_balance, account_to_balance, `timestamp`, `set`, gain, loss) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
             for (Object obj : data) {
@@ -97,8 +96,7 @@ public class Transactions {
             if(rs != null)
                 try { rs.close(); } catch (SQLException ex) { }
 
-            if(conn != null)
-                iConomy.getDatabase().checkIn(conn);
+            iConomy.getDatabase().close(conn);
         }
     }
 }
