442b6808104190fc4aa5c93bda482e542721a926
Nijiko Yonskai <me@nijikokun.com>
2011-06-24 02:45:01 -0400

    Finished /money remove <name> command!

--- src/com/iConomy/handlers/Remove.java
+++ src/com/iConomy/handlers/Remove.java
@@ -7,12 +7,15 @@ import com.iConomy.command.Parser.Argument;
 import com.iConomy.command.exceptions.InvalidUsage;
 
 import com.iConomy.iConomy;
+import com.iConomy.system.Accounts;
 
 import com.iConomy.util.Messaging;
+import com.iConomy.util.Template;
 
 import org.bukkit.command.CommandSender;
 
 public class Remove extends Handler {
+    private Accounts Accounts = new Accounts();
 
     public Remove(iConomy plugin) {
         super(plugin, plugin.Template);
@@ -20,11 +23,29 @@ public class Remove extends Handler {
 
     @Override
     public boolean perform(CommandSender sender, LinkedHashMap<String, Argument> arguments) throws InvalidUsage {
-        if(isConsole(sender))
-            Messaging.send(sender, "`rCannot check money on non-living organism.");
+        String name = arguments.get("name").getStringValue();
+        String tag = template.color(Template.Node.TAG_MONEY);
 
-        System.out.println();
+        if(name.equals("0"))
+            throw new InvalidUsage("Missing name parameter: /money remove <name>");
 
+        if(!Accounts.exists(name)) {
+            template.set(Template.Node.ERROR_ACCOUNT);
+            template.add("name", name);
+            Messaging.send(sender, tag + template.parse());
+            return false;
+        }
+
+        if(!Accounts.remove(name)) {
+            template.set(Template.Node.ERROR_CREATE);
+            template.add("name", name);
+            Messaging.send(sender, tag + template.parse());
+            return false;
+        }
+
+        template.set(Template.Node.ACCOUNTS_REMOVE);
+        template.add("name", name);
+        Messaging.send(sender, tag + template.parse());
         return false;
     }
 }
