f83c04a713f112b9910a908c1273ee7d576669a3
Nijiko Yonskai <me@nijikokun.com>
2011-06-24 02:08:59 -0400

    Finally fixing the casing on these classes.

---
+++ src/com/iConomy/handlers/Money.java
@@ -0,0 +1,56 @@
+package com.iConomy.handlers;
+
+import java.util.LinkedHashMap;
+
+import com.iConomy.command.Handler;
+import com.iConomy.command.Parser.Argument;
+import com.iConomy.command.exceptions.InvalidUsage;
+
+import com.iConomy.iConomy;
+import com.iConomy.system.Account;
+import com.iConomy.system.Accounts;
+
+import com.iConomy.util.Messaging;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Money extends Handler {
+
+    private Accounts Accounts = new Accounts();
+
+    public Money(iConomy plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean perform(CommandSender sender, LinkedHashMap<String, Argument> arguments) throws InvalidUsage {
+        String name = arguments.get("name").getStringValue();
+
+        if(name.equals("0")) {
+            if(isConsole(sender)) {
+                Messaging.send(sender, "`rCannot check money on non-living organism.");
+                return false;
+            }
+
+            Player player = (Player) sender;
+
+            if(player == null)
+                return false;
+
+            Account account = new Account(player.getName());
+            account.getHoldings().showBalance(null);
+            
+            return false;
+        }
+
+        if(!Accounts.exists(name)) {
+            Messaging.send(sender, "`rAccount for " + name + " does not exist!");
+            return false;
+        }
+
+        Account account = new Account(name);
+        account.getHoldings().showBalance(sender);
+        return false;
+    }
+}
---
+++ src/com/iConomy/handlers/Remove.java
@@ -0,0 +1,30 @@
+package com.iConomy.handlers;
+
+import java.util.LinkedHashMap;
+
+import com.iConomy.command.Handler;
+import com.iConomy.command.Parser.Argument;
+import com.iConomy.command.exceptions.InvalidUsage;
+
+import com.iConomy.iConomy;
+
+import com.iConomy.util.Messaging;
+
+import org.bukkit.command.CommandSender;
+
+public class Remove extends Handler {
+
+    public Remove(iConomy plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean perform(CommandSender sender, LinkedHashMap<String, Argument> arguments) throws InvalidUsage {
+        if(isConsole(sender))
+            Messaging.send(sender, "`rCannot check money on non-living organism.");
+
+        System.out.println();
+
+        return false;
+    }
+}
