c6df14e17c9d6591ba2fc56a97b39650cc4a696d
Nijiko Yonskai <me@nijikokun.com>
2011-06-24 11:15:54 -0400

    Removing prior sql files.

--- create-postgre.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "iconomy" (
-  "id" bigserial NOT NULL PRIMARY KEY,
-  "username" character varying(32) NOT NULL,
-  "balance" numeric(64,2) NOT NULL,
-  "status" smallint DEFAULT 0 NOT NULL,
-  CONSTRAINT "username" UNIQUE ("username")
-);
--- create-sqlite.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE iconomy (
-  id INTEGER PRIMARY KEY,
-  username varchar(32) NOT NULL,
-  balance double(64,2) NOT NULL,
-  status int(2) NOT NULL DEFAULT 0
-);
-CREATE UNIQUE INDEX username ON iconomy (username);
-COMMIT;
