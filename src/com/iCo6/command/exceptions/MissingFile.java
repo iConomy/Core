4bd13b35f4cdc6a91ffcb69a7fcb85b1da1d51a1
Nijiko Yonskai <me@nijikokun.com>
2011-09-17 01:46:11 -0400

    Colored missing file.

--- src/com/iCo6/command/exceptions/MissingFile.java
+++ src/com/iCo6/command/exceptions/MissingFile.java
@@ -2,6 +2,6 @@ package com.iCo6.command.exceptions;
 
 public class MissingFile extends Exception {
     public MissingFile(String file) {
-        super("File could not be found: " + file);
+        super("<rose>File could not be found: <white>" + file);
     }
 }
