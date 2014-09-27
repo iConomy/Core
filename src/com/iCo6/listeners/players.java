46e22f4b4fda84efcc28b93daebb42d7c0fb7db7
Liam <thetryhardnoob@gmail.com>
2014-09-27 15:43:49 +0100

    UUID
    Now using UUIDs

--- src/com/iCo6/listeners/players.java
+++ src/com/iCo6/listeners/players.java
@@ -14,7 +14,9 @@ public class players implements Listener {
         Player player = event.getPlayer();
 
         if(player != null)
-            if(!accounts.exists(player.getName()))
-                accounts.create(player.getName());
+            if(!accounts.exists(player.getUniqueId()))
+                accounts.create(player.getUniqueId());
+                
     }
+    
 }
